eaa5f293978a38f31a58cabb11c30d9b419ac1c8
Prashant Sinha <2052-prashant.sinha@users.noreply.source.golabs.io>
2021-04-26 23:55:45 +0530

    Revert "[fr33m0nk]: Adds clojar token"
    This reverts commit 8e4cd6df

--- project.clj
+++ project.clj
@@ -23,8 +23,7 @@
                                        [org.apache.kafka/kafka-streams "2.8.0" :classifier "test" :scope "provided"]
                                        [org.apache.kafka/kafka-clients "2.8.0" :classifier "test" :scope "provided"]]}
              :provided {:dependencies []}}
-  {:deploy-repositories [["releases" :clojars]
-                         ["snapshots" :clojars]]}
+  :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[kafka-cluster-test-utility \"[0-9.]*\"\\\\]/[kafka-cluster-test-utility \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
                   ["change" "version" "leiningen.release/bump-version"]

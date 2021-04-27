b2d77c7d8210551613308df96d4fa5226364bd86
Prashant Sinha <2052-prashant.sinha@users.noreply.source.golabs.io>
2021-04-27 08:37:15 +0530

    [fr33m0nk]: removes :pod-addition from project.clj

--- project.clj
+++ project.clj
@@ -5,9 +5,6 @@
             :url  "http://www.eclipse.org/legal/epl-v20.html"}
   :scm {:name "git"
         :url "https://github.com/fr33m0nk/kafka-cluster-test-utility"}
-  :pom-addition [:developers [:developer {:id "benbit"}
-                              [:name "Prashant"]
-                              [:url "http://www.example.com/benjamin"]]]
   :dependencies [[org.clojure/tools.namespace "1.1.0"]
                  [junit/junit "4.13.2"]
                  [org.junit.jupiter/junit-jupiter-api "5.7.1"]
